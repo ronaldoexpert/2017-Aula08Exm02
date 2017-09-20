89e9ab495c059fde3552aadb319d7400355d47d6
ice <ice@lab06m04.nrc.ice.ufjf.br>
2017-09-19 21:31:30 -0300

    Incio

---
+++ src/aula08exm02/Aluno.java
@@ -0,0 +1,39 @@
+package aula08exm02;
+
+public class Aluno {
+    private String matricula;
+    private String nome;
+    
+    public Aluno(){
+        this(null, null);
+    }
+    
+    public Aluno(String matricula){
+        this(matricula, null);
+    }
+
+    public Aluno(String matricula, String nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String toString(){
+        return matricula + ": "+ nome;
+    }
+}
